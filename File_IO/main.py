a92e00f741a8657a183136ec98133f0d6997977a
Jarrod Kassels <jarrod.kassels@gmail.com>
2023-02-11 13:38:14 -0800

    working with files

--- File_IO/main.py
+++ File_IO/main.py
@@ -7,3 +7,22 @@ with open("mydata.txt", encoding="utf-8") as my_file:
     print(my_file.read())
 
 print(my_file.closed)
+print(my_file.name)
+print(my_file.mode)
+
+with open("mydata.txt", encoding="utf-8") as my_file:
+    line_num = 1
+    while True:
+        line = my_file.readline()
+        if not line:
+            break
+        print("Line : ", line_num, " : ", line, end="")
+        word_list = line.split()
+        print("Number of words : ",len(word_list))
+        char_count = 0
+        for word in word_list:
+            for char in word:
+                char_count += 1
+        avg_num_chars = char_count/len(word_list)
+        print("Avg word Length: {:.2f}".format(avg_num_chars))
+        line_num +=1
