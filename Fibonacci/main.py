978fc93817aabc3548b57e94df2da968e2d899f2
Jarrod Kassels <jarrod.kassels@gmail.com>
2023-02-11 12:08:43 -0800

    fib sequence

---
+++ Fibonacci/main.py
@@ -0,0 +1,10 @@
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        result = fib(n - 1) + fib(n - 2)
+        return result
+print(fib(3))
+print(fib(4))
